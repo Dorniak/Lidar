686a31eff3cd30d161e480ab3c68f3892d872cdb
Dorniak <a.vbarrio@hotmail.com>
2015-10-01 10:35:13 +0200

    Obstaculo actualizado

--- Lidar/Obstaculo.cpp
+++ Lidar/Obstaculo.cpp
@@ -6,9 +6,39 @@ Obstaculo::Obstaculo() {
 
 
 }
-void  Obstaculo::calcCentro() {
+void Obstaculo::setDirection(Punto3D antPosition)
+{
+	//TODO::Calcular el vector direccion restando las componentesdel centro actual menos el centro anterior
+}
+void Obstaculo::setVelocity()
+{
+	//TODO::Calcular el modulo del vector direccion y multiplicarlo por el tiempo de barrido
+}
+Punto3D Obstaculo::getCenter()
+{
+	return Center;
+}
+Punto3D Obstaculo::getPCenter()
+{
+	return PrediceCenter;
+}
+int Obstaculo::getVelocity()
+{
+	return Velocity;
+}
+void Obstaculo::prepareObstacle()
+{
+	//TODO::Hacer el calculo del cubo,centro,bordes...
+}
+void  Obstaculo::calcCentro()
+{
 
 }
-void  Obstaculo::calcCentropred() {
+void  Obstaculo::calcCentropred()
+{
 
+}
+void Obstaculo::calcTTC()
+{
+	//TODO::Calcular el TTC a partir de la velocidad del coche y del veector direccion del obstaculo
 }
--- Lidar/Obstaculo.h
+++ Lidar/Obstaculo.h
@@ -5,9 +5,18 @@ public class Obstaculo
 {
 public:
 	Obstaculo();
-	double get;
-	void calcCentro();
-	void calcCentropred();
+
+	void setDirection(Punto3D antPosition);//Pone la direccion al obstaculo
+	void setVelocity();//Pone la velocidad a partir del modulo del vector de direccion
+
+	Punto3D getCenter();//Devuelve el centro del obstaculo
+	Punto3D getPCenter();//Devuelve el centro predicho del obstaculo
+	int getVelocity();//Devuelve a la velocidad a la que va el obstaculo
+
+	void prepareObstacle();//Calcula los datos del obstaculo
+	void calcCentro();//Calcula el centro del obstaculo
+	void calcCentropred();//Calcula el centro predicho a partir de la velocidad del coche y el vector de direccion
+	void calcTTC();//Calcula el tiempo de colision del obstaculo con el coche
 	std::vector <Punto3D> componentes;
 private:
 	Punto3D* North;
@@ -15,4 +24,6 @@ private:
 	Punto3D* East;
 	Punto3D* West;
 	Punto3D Center;
+	Punto3D PrediceCenter;
+	int Velocity;
 };
