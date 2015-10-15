476e3ca4dd014c87036e5e25b1c442a2e5a6890b
Dorniak <a.vbarrio@hotmail.com>
2015-10-15 16:34:59 +0200

    lo de hoy puesto otra vez para eliminar incoherencias

--- Lidar/Obstaculo.h
+++ Lidar/Obstaculo.h
@@ -7,34 +7,38 @@ ref class Obstaculo
 public:
 	Obstaculo();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 #pragma region
-=======
-	void setDirection(Punto3D^ antPosition);//Pone la direccion al obstaculo
-	void setVelocity();//Pone la velocidad a partir del modulo del vector de direccion
-
->>>>>>> origin/master
-=======
-	void setDirection(Punto3D antPosition);//Pone la direccion al obstaculo
-	void setVelocity();//Pone la velocidad a partir del modulo del vector de direccion
-
->>>>>>> parent of 30cb8e0... Punto y Obstaculos totalmente implentados
 	Punto3D^ getCenter();//Devuelve el centro del obstaculo
-	Punto3D^ getPCenter();//Devuelve el centro predicho del obstaculo
-	int getVelocity();//Devuelve a la velocidad a la que va el obstaculo
+	Punto3D^ getPrediceCenter();//Devuelve el centro predicho del obstaculo
+	Punto3D^ getNorth();
+	Punto3D^ getSouth();
+	Punto3D^ getEast();
+	Punto3D^ getWest();
+#pragma endregion Getters
 
-	void prepareObstacle();//Calcula los datos del obstaculo
-	void calcCentro();//Calcula el centro del obstaculo
-	void calcCentropred();//Calcula el centro predicho a partir de la velocidad del coche y el vector de direccion
-	void calcTTC();//Calcula el tiempo de colision del obstaculo con el coche
-	cliext::vector <Punto3D^> componentes ;
+#pragma region
+	void setNorth(Punto3D^ p);
+	void setSouth(Punto3D^ p);
+	void setEast(Punto3D^ p);
+	void setWest(Punto3D^ p);
+#pragma endregion Setters
+
+
+	void setDirection(Punto3D^ antPosition);//Pone la direccion al obstaculo
+	void setVelocity(double Car_velocity, double Frecuency);//Pone la velocidad a partir del modulo del vector de direccion
+	int getVelocity();//Devuelve a la velocidad a la que va el obstaculo
+	void calculateCenter();//Calcula el centro del obstaculo
+	void calculatePrediceCenter();//Calcula el centro predicho a partir de la velocidad del coche y el vector de direccion
+	void calculateTimeToCollision();//Calcula el tiempo de colision del obstaculo con el coche
+
 private:
 	Punto3D^ North;;
 	Punto3D^ South;
 	Punto3D^ East;
 	Punto3D^ West;
 	Punto3D^ Center;
-	Punto3D^ PrediceCenter;
-	int Velocity;
+	Punto3D^ Predice_Center;
+	Punto3D^ Direction;
+	double Velocity;
+	cliext::vector <Punto3D^> components;
 };
--- Lidar/Punto3D.h
+++ Lidar/Punto3D.h
@@ -1,72 +1,58 @@
 #pragma once
-
+#include <math.h>
 using namespace System;
 using namespace std;
 
 ref class Punto3D
-	{
-	public:
-		Punto3D(double d, double i, double r);
-		Punto3D();
+{
+public:
+	Punto3D(double d, double i, double r);
+	Punto3D();
 
 #pragma region
-
-		void setExist(bool ex);
-		void setObstacle(int Obs);
-		void setCoordinatesX(double cx);
-		void setCoordinatesY(double cy);
-		void setCoordinatesZ(double cz);
-		void setDistance(double d);
-		void setIntensity(double i);
-		void setAzimuth(double r);
-		void setDistance(Byte i, Byte j);
-		void setIntensity(Byte i);
-		void setAzimuth(Byte i, Byte j);
-		void setAngle(int channel);
+	void setObstacle(int Obs);
+	void setCoordinatesX(double cx);
+	void setCoordinatesY(double cy);
+	void setCoordinatesZ(double cz);
+	void setDistance(double d);
+	void setIntensity(double i);
+	void setAzimuth(double r);
+	void setDistance(Byte i, Byte j);
+	void setIntensity(Byte i);
+	void setAzimuth(Byte i, Byte j);
+	void setAngle(int channel);
 #pragma endregion Setters
 
 #pragma region
-		bool getExists();
-		int getObs();
-		double getCoordinatesX();
-		double getCoordinatesY();
-		double getCoordinatesZ();
-		double getDistance();
-		double getIntensity();
-		double getAzimuth();
-		double getAngle();
+	int getObs();
+	double getCoordinatesX();
+	double getCoordinatesY();
+	double getCoordinatesZ();
+	double getDistance();
+	double getIntensity();
+	double getAzimuth();
+	double getAngle();
+	double getModule();
 #pragma endregion Getters
 
 #pragma region
-<<<<<<< HEAD
-<<<<<<< HEAD
-		void CalculateCoordenates();
-		double distanceToPoint(Punto3D p);
-		Punto3D^ operator-(Punto3D ^ v);
-		Punto3D^ operator+(Punto3D ^ v);
-		Punto3D^ operator*(double d);
-=======
-		void visualize();
-		double distanceToPoint(Punto3D^ p);
->>>>>>> origin/master
-=======
-		void visualize();
-		double distanceToPoint(Punto3D p);
->>>>>>> parent of 30cb8e0... Punto y Obstaculos totalmente implentados
+	void CalculateCoordenates();
+	double distanceToPoint(Punto3D p);
+	Punto3D^ operator-(Punto3D ^ v);
+	Punto3D^ operator+(Punto3D ^ v);
+	Punto3D^ operator*(double d);
 #pragma endregion Others
 
 #pragma region
-	private:
-		bool Exist;			// The point exist or not
-		int Obstacle;		// Obstacle that contains the point
-		double Intensity;	// Bounced beam intensity
-		double Distance;	// Distance to the point
-		double Azimuth;		// Horizontal angle Y-X
-		double Angle;		// Vertical angle Laser Id Z-Y
-		double x;			// X coordinate of the point
-		double y;			// Y coordinate of the point	
-		double z;			// Z coordinate of the point
-	};
+private:
+	int Obstacle;		// Obstacle that contains the point
+	double Intensity;	// Bounced beam intensity
+	double Distance;	// Distance to the point
+	double Azimuth;		// Horizontal angle Y-X
+	double Angle;		// Vertical angle Laser Id Z-Y
+	double x;			// X coordinate of the point
+	double y;			// Y coordinate of the point	
+	double z;			// Z coordinate of the point
+};
 
 #pragma endregion Propierties
-
