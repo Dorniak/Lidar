e7c8c30b0ba4844bb5ea612c7799acdb8e094508
Dorniak <a.vbarrio@hotmail.com>
2015-10-16 16:27:51 +0200

    prueba

--- Lidar/pruebas.cpp
+++ Lidar/pruebas.cpp
@@ -6,6 +6,8 @@ void Main(cli::array<String^>^ args)
 {
 	Punto3D^ p = gcnew Punto3D();
 	p->setDistance(5);
-
+	Punto3D aa;
+	aa.setDistance(10);
+	Console::WriteLine("distancia {0}", aa.getDistance());
 	Console::WriteLine("distancia {0}",p->getDistance());
 }
